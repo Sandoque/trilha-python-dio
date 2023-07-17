eca28ffe6de2da3b79bc9c6fdc7a22302717f6db
Antonio Carlos Sandoque <acsandoque@gmail.com>
2023-07-16 22:23:51 -0300

    atu listas

--- 01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
+++ 01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
@@ -1,8 +1,15 @@
 carros = ["gol", "celta", "palio"]
 
-for carro in carros:
-    print(carro)
+# for carro in carros:
+#     print(carro)
 
+
+# for indice, carro in enumerate(carros):
+#     print(f"{indice}: {carro}")
+
+# teste = enumerate(carros)
+# for i in teste:
+#     print(f"{int(i[0])+1} - {i[1]}")
 
 for indice, carro in enumerate(carros):
-    print(f"{indice}: {carro}")
+    print(f"{indice+1}: {carro}")
--- 01 - Estrutura de dados/01 - Listas/08_clear.py
+++ 01 - Estrutura de dados/01 - Listas/08_clear.py
@@ -1,4 +1,5 @@
 lista = [1, "Python", [40, 30, 20]]
+print(id(lista))
 
 print(lista)  # [1, "Python", [40, 30, 20]]
 
